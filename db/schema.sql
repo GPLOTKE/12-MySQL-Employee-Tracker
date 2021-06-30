5ac2bf7e27187da034180ad42de9d58a2f36dbdc
Jerry Plotke <jerry.plotke@gmail.com>
2021-06-30 00:15:54 -0500

    creating tables

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,14 @@
 DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id)
+);
